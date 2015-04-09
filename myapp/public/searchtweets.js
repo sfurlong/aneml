0054245b3e75173defa6aa1b2217e0caccbf22ca
sfurlong <steveinvienna@gmail.com>
2015-04-09 12:38:05 -0400

    bootstrap update

--- myapp/public/searchtweets.js
+++ myapp/public/searchtweets.js
@@ -3,10 +3,11 @@ angular.module("mainModule", [])
   {
     var serverURL = "http://localhost:3000";
 
-    var logResult = function (str, data, status, headers, config)
+    var logResult = function (str, data, status, headers, config, cnt)
     {
       return str + "\n\n" +
-        "data: " + data + "\n\n" +
+        "Num Returned: " + cnt + "\n\n" +
+        "data: " + '\n\n' + data + "\n\n" +
         "status: " + status + "\n\n" +
         "headers: " + jsonFilter(headers()) + "\n\n" +
         "config: " + jsonFilter(config);
@@ -33,13 +34,32 @@ angular.module("mainModule", [])
 			// JavaScript
           // object whenever it sees that it looks like a JSON string.
           //console.log(data);
-          data = jsonFilter(data);
+          var tweetCnt = 0;
+          var ret = '';
+          data.forEach( function(document) {
+              console.log(document.content.text);
+              console.log(document.content.user.friends_count);
+              console.log(document.content.user.screen_name);
+              console.log(document.content.user.name);
+              console.log(document.content.user.location);
+              ret += document.content.text + '\n' +
+                document.content.user.friends_count + '\n' +
+                document.content.user.screen_name + '\n' +
+                document.content.user.name + '\n' +
+                document.content.user.location + 
+                  '\n---------------------------------\n';
+              tweetCnt++;
+//          console.log('Name: ' + document.content.name); 
+          });
+          
+          data = ret;
+          //data = jsonFilter(data);
 
-          $scope.getCallJSONResult = logResult("GET SUCCESS2", data, status, headers, config);
+          $scope.getCallJSONResult = logResult("GET SUCCESS", data, status, headers, config, tweetCnt);
         })
         .error(function (data, status, headers, config)
         {
-          $scope.getCallJSONResult = logResult("GET ERROR2", data, status, headers, config);
+          $scope.getCallJSONResult = logResult("GET ERROR2", data, status, headers, config, 0);
         });
     };
   });
