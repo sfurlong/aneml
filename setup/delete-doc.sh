ead33a15cf345cf47d73136ca1a81797ffece74c
sfurlong <steveinvienna@gmail.com>
2015-04-29 17:10:40 -0400

    updates

--- setup/delete-doc.sh
+++ setup/delete-doc.sh
@@ -1,6 +1,6 @@
 ADMIN_UID=admin
 ADMIN_PWD=marklogic1
 ML_ADMIN_HOST=localhost
-ML_DATA_PORT=7010
-curl --anyauth --user admin:marklogic1 -X DELETE "http://localhost:7010/v1/documents?uri=/songs/song3.xml"
+ML_DATA_PORT=9010
+curl --anyauth --user admin:marklogic1 -X DELETE "http://$ML_ADMIN_HOST:$ML_DATA_PORT/v1/documents?uri=/tweets/572773090364674048.json"
 
