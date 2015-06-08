c98d084bf0f1d6a7765560a94f0d7183a3d6edf4
sfurlong <steveinvienna@gmail.com>
2015-06-08 13:28:20 -0400

    random updates

--- setup/searchML.sh
+++ setup/searchML.sh
@@ -1,13 +1,8 @@
+#
 ADMIN_UID=admin
 ADMIN_PWD=marklogic1
 ML_HOST=localhost
 ML_DATA_PORT=9010
-#
-# REST API call to search the database for any document that contains the word "bigdata", returning the results as JSON
-#curl --anyauth --user admin:marklogic1 -X GET http://localhost:9010/v1/search?q=bigdata&format=json
 
 # REST API call to search the database for any document that contains the word "bigdata", returning the results as JSON
-#curl --anyauth --user admin:marklogic1 -X GET http://localhost:9010/v1/search?q=bigdata&format=json&category=content
-
-# REST API call to search the database for any document that contains the word "bigdata", returning the results as JSON
-curl --anyauth --user admin:marklogic1 -X GET -i -i "Accept: multipart/mixed; boundary=BOUNDARY" http://localhost:9010/v1/search?q=bigdata&format=json&view=results
+curl --anyauth --user $ADMIN_UID:$ADMIN_PWD -X GET -i -H "Accept: multipart/mixed; boundary=BOUNDARY" http://$ML_HOST:$ML_DATA_PORT/v1/search?q=bigdata&format=json&view=results
